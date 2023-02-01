ba45308911243e5fb0a627b0df50e80192769956
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-02-01 20:12:38 +0100

    Day 8 (Ocaml) Refactor

--- d8_ocaml/main.ml
+++ d8_ocaml/main.ml
@@ -2,78 +2,71 @@ module Solution = struct
   open Array
   type direction = Up | Down | Left | Right
 
+  let iter_grid grid fn =
+    let rows = length grid in
+    let cols = length grid.(0) in
+    for col = 0 to cols - 1 do
+      for row = 0 to rows - 1 do
+        fn (row, col)
+      done
+    done
+
   let look dir grid row col =
     let rows = length grid in
     let cols = length grid.(0) in
     let count = ref 0 in
-    try
-      match dir with
-      | Up ->
-          for i = col - 1 downto 0 do
-            if grid.(i).(row) < grid.(col).(row) then incr count else raise Exit
-          done;
-          (!count, true)
-      | Down ->
-          for i = col + 1 to rows - 1 do
-            if grid.(i).(row) < grid.(col).(row) then incr count else raise Exit
-          done;
-          (!count, true)
-      | Left ->
-          for i = row - 1 downto 0 do
-            if grid.(col).(i) < grid.(col).(row) then incr count else raise Exit
-          done;
-          (!count, true)
-      | Right ->
-          for i = row + 1 to cols - 1 do
-            if grid.(col).(i) < grid.(col).(row) then incr count else raise Exit
-          done;
-          (!count, true)
-    with Exit ->
-      incr count;
-      (!count, false)
+    let visible = ref true in
+
+    let loop start limit inc comparison =
+      let i = ref start in
+      while !i <> limit && !visible do
+        i := !i + inc;
+        if comparison i < grid.(col).(row) then incr count
+        else (
+          incr count;
+          visible := false)
+      done;
+      (!count, !visible)
+    in
+
+    match dir with
+    | Up -> loop col 0 (-1) (fun x -> grid.(!x).(row))
+    | Down -> loop col (cols - 1) 1 (fun x -> grid.(!x).(row))
+    | Left -> loop row 0 (-1) (fun x -> grid.(col).(!x))
+    | Right -> loop row (rows - 1) 1 (fun x -> grid.(col).(!x))
 
   let count_visible_trees grid =
-    let rows = length grid in
-    let cols = length grid.(0) in
     let count_visible = ref 0 in
-    for col = 0 to cols - 1 do
-      for row = 0 to rows - 1 do
-        if col = 0 || row = 0 || col = cols - 1 || row = rows - 1 then
-          incr count_visible
-        else if
+    iter_grid grid (fun (row, col) ->
+        if
           snd (look Up grid row col)
           || snd (look Down grid row col)
           || snd (look Left grid row col)
           || snd (look Right grid row col)
-        then incr count_visible
-      done
-    done;
+        then incr count_visible);
     !count_visible
 
   let count_scenic_score grid =
-    let rows = length grid in
-    let cols = length grid.(0) in
     let max_scenic_score = ref 0 in
-    for col = 0 to cols - 1 do
-      for row = 0 to rows - 1 do
+    iter_grid grid (fun (row, col) ->
         let scenic_score =
           fst (look Up grid row col)
           * fst (look Down grid row col)
           * fst (look Left grid row col)
           * fst (look Right grid row col)
         in
-        max_scenic_score := max !max_scenic_score scenic_score
-      done
-    done;
+        max_scenic_score := max !max_scenic_score scenic_score);
     !max_scenic_score
 
   let part1 grid =
     let result = count_visible_trees grid in
+    print_string "Part 1: ";
     print_int result;
     print_newline ()
 
   let part2 grid =
     let result = count_scenic_score grid in
+    print_string "Part 2: ";
     print_int result;
     print_newline ()
 end
