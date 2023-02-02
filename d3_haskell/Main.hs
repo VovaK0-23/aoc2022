00a8048e7f08a1434ba30fcd72dbb505bae0e4f1
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-02-02 22:11:42 +0100

    Day 3 (Haskell) Add comments and refactor

--- d3_haskell/Main.hs
+++ d3_haskell/Main.hs
@@ -1,50 +1,66 @@
 module Main where
 
+import Data.Char (isLower, ord)
+import Data.Function ((&))
+import System.Environment (getArgs)
 import System.IO
-import System.Environment
-import Data.Char
-import Data.Function
-import Text.Printf
+import Text.Printf (printf)
 
+main :: IO ()
 main = do
   args <- getArgs
   case args of
-    (filePath:_) -> do
+    -- check if input file is provided or not
+    (filePath : _) -> do
+      -- open input file
       handle <- openFile filePath ReadMode
+      -- read contents of the file
       contents <- hGetContents handle
+      -- print the result
       printf "Part 1: %d\n" $ part1 contents
       printf "Part 2: %d\n" $ part2 contents
+      -- close the input file
       hClose handle
+    -- if there is no argument passed, raise an error message
     _ -> error "Input file is not provided"
 
-
+-- The part1 function takes the contents of the input file as a string,
+-- splits the contents into words,
+-- finds the intersections of words,
+-- calculates the priority of the first character of the intersected word,
+-- and returns the sum of all priorities.
 part1 :: String -> Int
 part1 contents =
-  sumList $ map (priority . intersect . split) $ words contents
+  sum $ map (priority . intersect . split) $ words contents
 
+-- The part2 function is similar to part1,
+-- but it groups the words into sublists of three
+-- and finds the intersections of words from these sublists.
+part2 :: String -> Int
 part2 contents =
-  sumList $ map (priority . intersect) $ words contents & groupBy 3
+  sum $ map (priority . intersect) . groupBy 3 $ words contents
 
-priority :: String -> Int
-priority (char:_) | isLower char = ord char - ord 'a' + 1
-                    | otherwise = ord char - ord 'A' + 27
+priority :: Char -> Int
+priority char
+  | isLower char = ord char - ord 'a' + 1
+  | otherwise = ord char - ord 'A' + 27
 
-intersect :: [String] -> String
+-- The intersect function takes a list of strings
+-- and returns the first character of the string that is present in all other strings.
+intersect :: [String] -> Char
 intersect xs = case xs of
-  (a:b:c:_) -> f (\x -> elem x b && elem x c) a
-  (a:b:_) -> f (\x -> elem x b) a
-  where f x a =  (filter x a) & take 1
-
-sumList :: [Int] -> Int
-sumList [] = 0
-sumList (x:xs) = x + sumList xs
+  (a : b : c : _) -> head (f (\x -> elem x b && elem x c) a)
+  (a : b : _) -> head (f (`elem` b) a)
+  where
+    f x a = filter x a & take 1
 
 split :: [a] -> [[a]]
 split myList =
-        [(take listLen myList), (drop listLen myList)]
-        where listLen =  length(myList) `div` 2
+  [take listLen myList, drop listLen myList]
+  where
+    listLen = length myList `div` 2
 
+-- Groups elements of a list into sublists of a specified length
 groupBy :: Int -> [a] -> [[a]]
 groupBy n [] = []
-groupBy n xs = (take n xs):(groupBy n (drop n xs))
-
+groupBy n xs = take n xs : groupBy n (drop n xs)
