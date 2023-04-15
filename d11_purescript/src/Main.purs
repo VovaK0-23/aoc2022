f77aef97a36df6c21efc32bd8c6c264c2f427d81
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-04-15 05:38:07 +0200

    Day 11 (PureScript) - Refactor

--- d11_purescript/src/Main.purs
+++ d11_purescript/src/Main.purs
@@ -2,13 +2,12 @@ module Main where
 
 import Prelude
 
-import Data.Array as Array
+import Data.Array (drop, foldl, replicate, reverse, sort, take, (!!))
 import Data.Either (Either(..))
 import Data.Int as Int
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (fromMaybe)
 import Data.Result (Result(..))
-import Data.Traversable (traverse)
-import Data.Tuple (Tuple(..))
+import Data.Tuple (uncurry)
 import Effect (Effect)
 import Effect.Console (error, log)
 import Effect.Exception (message, try)
@@ -17,7 +16,7 @@ import JS.BigInt as BigInt
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 import Node.Process (argv)
-import Parser (Monkey, mkMonkeys)
+import Parser (Id, WorryLevel, Monkey, mkMonkeys)
 
 {-
 Why I used BigInt?
@@ -26,78 +25,22 @@ I can just summon the infinite power of BigInts to do my bidding?
 Besides, who needs moduli when you have an infinitely large integer?
 
 In the end, I used both modular calculations and BigInt.
-Without modular calculations, it would have taken forever, but unfortunately,
-I couldn't make the numbers any smaller than a regular Int in PureScript.
+Without modular calculations, it would have taken forever to run, but unfortunately,
+I couldn't make the numbers small enough to fit a regular Int in PureScript.
 I guess I'll just have to settle for being a mere mortal programmer instead of a wizard.
 -}
-moveItem :: Array Monkey -> Int -> Array Monkey
-moveItem monkeys from = fromMaybe monkeys $ do
-  fromMonkey <- Array.find (\m -> m.id == from) monkeys
-  Tuple worryLevel id <- Array.head fromMonkey.itemsInspected
-  toMonkey <- Array.find (\m -> m.id == id) monkeys
-  let
-    newFromMonkey = fromMonkey { itemsInspected = Array.drop 1 fromMonkey.itemsInspected }
-    newToMonkey = toMonkey { items = toMonkey.items <> [ worryLevel ] }
-  traverse (\m -> if m.id == id then Just newToMonkey else Just m) monkeys
-    >>= traverse (\m -> if m.id == from then Just newFromMonkey else Just m)
 
-inspectItems1 :: Monkey -> Monkey
-inspectItems1 monkey =
-  Array.foldl
-    ( \monkey' item ->
-        monkey'
-          { itemsCount = monkey'.itemsCount + BigInt.fromInt 1
-          , items = Array.drop 1 monkey'.items
-          , itemsInspected = monkey'.itemsInspected <>
-              [ (monkey'.test $ BigInt.fromInt ((fromMaybe 0 $ Int.fromString $ BigInt.toString (monkey'.op item)) / 3)) ]
-          }
-    )
-    monkey
-    monkey.items
+main :: Effect Unit
+main = do
+  args <- argv
+  case args of
+    [ _, _, filePath ] -> do
+      fileContentsEither <- try $ readTextFile UTF8 filePath
 
-inspectItems2 :: Monkey -> BigInt -> Monkey
-inspectItems2 monkey modulo' =
-  Array.foldl
-    ( \monkey' item ->
-        monkey'
-          { itemsCount = monkey'.itemsCount + BigInt.fromInt 1
-          , items = Array.drop 1 monkey'.items
-          , itemsInspected = monkey'.itemsInspected <>
-              [ (monkey'.test (((monkey'.op item) `mod` modulo'))) ]
-          }
-    )
-    monkey
-    monkey.items
-
-modulo :: Array Monkey -> Int
-modulo monkeys = Array.foldl (\acc m -> m.devisor * acc) 1 monkeys
-
-runRounds :: Int -> Array Monkey -> Int -> Array Monkey
-runRounds numRounds monkeys part = Array.foldr (\_ ms -> runRound ms part) monkeys (Array.replicate numRounds unit)
-
-runRound :: Array Monkey -> Int -> Array Monkey
-runRound monkeys part =
-  Array.foldl
-    ( \monkeys' monkey ->
-        let
-          monkeyCurrent = fromMaybe monkey $ Array.find (\m -> m.id == monkey.id) monkeys'
-          monkeyInspected = if part == 1 then inspectItems1 monkeyCurrent else inspectItems2 monkeyCurrent (BigInt.fromInt (modulo monkeys'))
-          itemsInspected = monkeyInspected.itemsInspected
-          monkeys'' = map (\m -> if m.id == monkey.id then monkeyInspected else m) monkeys'
-        in
-          Array.foldl
-            ( \ms _ ->
-                moveItem ms monkey.id
-            )
-            monkeys''
-            itemsInspected
-    )
-    monkeys
-    monkeys
-
-monkeyBusiness :: Array Monkey -> BigInt
-monkeyBusiness monkeys =
-  Array.foldl (*) (BigInt.fromInt 1) $ Array.take 2 $ Array.reverse $ Array.sort $ map (\m -> m.itemsCount) monkeys
+      case fileContentsEither of
+        Left err -> error $ message $ err
+        Right fileContents -> solve fileContents
+    _ -> log "Usage: spago run -b '<input-path>'"
 
 solve :: String -> Effect Unit
 solve fileContents = do
@@ -108,15 +51,34 @@ solve fileContents = do
       let part2 = show $ monkeyBusiness $ runRounds 10000 monkeys 2
       log $ "Part 1: " <> part1
       log $ "Part 2: " <> part2
+  where
+  monkeyBusiness monkeys =
+    foldl (*) (BigInt.fromInt 1) $ take 2 $ reverse $ sort $ map (\m -> m.itemsCount) monkeys
 
-main :: Effect Unit
-main = do
-  args <- argv
-  case args of
-    [ _, _, filePath ] -> do
-      fileContentsEither <- try $ (readTextFile UTF8 filePath)
+runRounds :: Int -> Array Monkey -> Int -> Array Monkey
+runRounds numRounds monkeys part =
+  foldl (\ms _ -> runRound ms) monkeys $ replicate numRounds unit
+  where
+  modulo = BigInt.fromInt $ foldl (\acc m -> m.devisor * acc) 1 monkeys
+  inspectFn wl =
+    if part == 1 then
+      BigInt.fromInt $ (fromMaybe 0 $ Int.fromString $ BigInt.toString wl) / 3
+    else wl
+  runRound ms' = foldl (inspectItems inspectFn modulo) ms' ms'
 
-      case fileContentsEither of
-        Left err -> error $ message $ err
-        Right fileContents -> solve fileContents
-    _ -> log "Usage: spago run -b '<input-path>'"
+inspectItems :: (WorryLevel -> WorryLevel) -> BigInt -> Array Monkey -> Monkey -> Array Monkey
+inspectItems fn modulo monkeys monkey =
+  foldl inspectItem monkeys items
+  where
+  items = (fromMaybe monkey $ monkeys !! monkey.id).items
+  inspectItem monkeys' item =
+    uncurry (moveItem monkeys' monkey.id) $ monkey.test $ fn $ monkey.op item `mod` modulo
+
+moveItem :: Array Monkey -> Id -> Id -> WorryLevel -> Array Monkey
+moveItem monkeys from to item = fromMaybe monkeys do
+  fromMonkey <- monkeys !! from
+  toMonkey <- monkeys !! to
+  let fromMonkey' = fromMonkey { items = drop 1 fromMonkey.items, itemsCount = fromMonkey.itemsCount + BigInt.fromInt 1 }
+  let toMonkey' = toMonkey { items = toMonkey.items <> [ item ] }
+  pure $ map (\m -> if m.id == from then fromMonkey' else if m.id == to then toMonkey' else m) monkeys
+
--- d11_purescript/src/Parser.purs
+++ d11_purescript/src/Parser.purs
@@ -3,26 +3,24 @@ module Parser (mkMonkeys, Monkey, Id, WorryLevel) where
 import Prelude
 
 import Data.Array as Array
-import JS.BigInt (BigInt)
-import JS.BigInt as BigInt
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
 import Data.Result (Result(..))
 import Data.String (split)
-import Data.String.Extra as StringEx
 import Data.String.Pattern (Pattern(..))
 import Data.String.Utils as String
 import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..), fst, snd)
+import JS.BigInt (BigInt)
+import JS.BigInt as BigInt
 
 type Id = Int
 type WorryLevel = BigInt
 type Monkey =
   { id :: Id
   , items :: Array WorryLevel
-  , itemsInspected :: Array (Tuple WorryLevel Id)
   , op :: WorryLevel -> WorryLevel
-  , test :: WorryLevel -> Tuple WorryLevel Id
+  , test :: WorryLevel -> Tuple Id WorryLevel
   , devisor :: Int
   , itemsCount :: BigInt
   }
@@ -31,54 +29,49 @@ isDivisibleBy :: Int -> BigInt -> Boolean
 isDivisibleBy x y = y `mod` BigInt.fromInt x == BigInt.fromInt 0
 
 parseLastInt :: String -> Maybe Int
-parseLastInt str = (Just $ StringEx.words str) >>= Array.last >>= Int.fromString
+parseLastInt str = (Just $ String.words str) >>= Array.last >>= Int.fromString
 
 parseItems :: String -> Array WorryLevel
-parseItems input = Array.mapMaybe BigInt.fromString $ Array.drop 2 $ StringEx.words input
+parseItems input = Array.mapMaybe BigInt.fromString $ Array.drop 2 $ String.words $ String.filter (\s -> s /= ",") input
 
 parseOp :: String -> Maybe (WorryLevel -> WorryLevel)
 parseOp input =
-  let
-    op = Array.index (String.words input) 5
-    val = Array.last $ StringEx.words input
-    valInt = BigInt.fromString =<< val
-    opFn = case op of
-      Just "*" -> Just (\val' old -> old * val')
-      Just "+" -> Just (\val' old -> old + val')
-      _ -> Nothing
-  in
-    case opFn of
-      Just fn ->
-        case Tuple val valInt of
-          Tuple (Just _) (Just valInt') -> Just (fn valInt')
-          Tuple (Just _) (Nothing) -> Just (\old -> fn old old)
-          _ -> Nothing
-      Nothing -> Nothing
+  case opFn of
+    Just fn ->
+      case valInt of
+        (Just valInt') -> Just (fn valInt')
+        (Nothing) -> Just (\old -> fn old old)
+    _ -> Nothing
+  where
+  val = Array.last $ String.words input
+  valInt = BigInt.fromString =<< val
+  opFn = case Array.index (String.words input) 5 of
+    Just "*" -> Just (*)
+    Just "+" -> Just (+)
+    _ -> Nothing
 
-parseTest :: String -> String -> String -> Result String (Tuple Int (WorryLevel -> Tuple WorryLevel Id))
-parseTest devisorL trueIdL falseIdL = do
-  let
-    devisor = parseLastInt devisorL
-    trueId = parseLastInt trueIdL
-    falseId = parseLastInt falseIdL
-  case (devisor) of
-    Just d ->
-      case (Tuple trueId falseId) of
-        Tuple (Just t) (Just f) -> Ok
-          ( Tuple d
-              ( \worryLevel ->
-                  if isDivisibleBy d (worryLevel) then Tuple (worryLevel) t
-                  else Tuple (worryLevel) f
-              )
-          )
-        Tuple (Nothing) (_) -> Error trueIdL
-        Tuple (_) (Nothing) -> Error falseIdL
-    _ -> Error devisorL
+parseTest :: String -> String -> String -> Result String (Tuple Int (WorryLevel -> Tuple Id WorryLevel))
+parseTest devisorL trueIdL falseIdL =
+  case ([ trueId, falseId, devisor ]) of
+    [ Just t, Just f, Just d ] -> Ok (Tuple d testFn)
+      where
+      testFn worryLevel =
+        if isDivisibleBy d (worryLevel) then Tuple t worryLevel
+        else Tuple f worryLevel
+
+    [ Nothing, _, _ ] -> Error trueIdL
+    [ _, Nothing, _ ] -> Error falseIdL
+    [ _, _, Nothing ] -> Error devisorL
+    _ -> Error (devisorL <> "\n  |>" <> trueIdL <> "\n  |>" <> falseIdL)
+  where
+  devisor = parseLastInt devisorL
+  trueId = parseLastInt trueIdL
+  falseId = parseLastInt falseIdL
 
 mkMonkey :: Tuple Int String -> Result String Monkey
 mkMonkey (Tuple index input) =
   let
-    lines = split (Pattern "\n") input
+    lines = String.lines input
   in
     case lines of
       [ _, itemsLine, opLine, tl1, tl2, tl3 ] ->
@@ -88,19 +81,20 @@ mkMonkey (Tuple index input) =
           test = parseTest tl1 tl2 tl3
         in
           case op of
-            Just op ->
+            Just op' ->
               case test of
-                Ok test -> Ok
+                Ok test' -> Ok
                   { id: index
                   , items: items
-                  , itemsInspected: []
-                  , op: op
-                  , devisor: fst test
-                  , test: snd test
+                  , op: op'
+                  , devisor: fst test'
+                  , test: snd test'
                   , itemsCount: BigInt.fromInt 0
                   }
-                Error line -> Error $ "Monkey: " <> show index <> "\n  " <> "Malformed input: could not parse Test:\n  |>" <> line
-            Nothing -> Error $ "Monkey: " <> show index <> "\n  " <> "Malformed input: could not parse Operation:\n  |>" <> opLine
+                Error line ->
+                  Error $ "Monkey: " <> show index <> "\n  " <> "Malformed input: could not parse Test:" <> "\n  |>" <> line
+            Nothing ->
+              Error $ "Monkey: " <> show index <> "\n  " <> "Malformed input: could not parse Operation:" <> "\n  |>" <> opLine
       _ -> Error $ "Monkey: " <> show index <> "\n  " <> "Malformed input: 6 lines expected to make Monkey"
 
 mkMonkeys :: String -> Result String (Array Monkey)
