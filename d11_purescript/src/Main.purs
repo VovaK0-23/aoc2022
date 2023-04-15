cdaeb48f0e55a7da43f1791e90079c19a78e61ff
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-04-15 07:46:46 +0200

    Day 11 (PureScript) Add comments

--- d11_purescript/src/Main.purs
+++ d11_purescript/src/Main.purs
@@ -55,30 +55,48 @@ solve fileContents = do
   monkeyBusiness monkeys =
     foldl (*) (BigInt.fromInt 1) $ take 2 $ reverse $ sort $ map (\m -> m.itemsCount) monkeys
 
+-- This function runs the specified number of rounds of the monkey business game,
+-- using the provided array of Monkey objects and either the part 1 or part 2 rules.
 runRounds :: Int -> Array Monkey -> Int -> Array Monkey
 runRounds numRounds monkeys part =
-  foldl (\ms _ -> runRound ms) monkeys $ replicate numRounds unit
-  where
-  modulo = BigInt.fromInt $ foldl (\acc m -> m.devisor * acc) 1 monkeys
-  inspectFn wl =
-    if part == 1 then
-      BigInt.fromInt $ (fromMaybe 0 $ Int.fromString $ BigInt.toString wl) / 3
-    else wl
-  runRound ms' = foldl (inspectItems inspectFn modulo) ms' ms'
+  -- First, compute the modulo value by multiplying the devisors of all the monkeys.
+  let modulo = BigInt.fromInt $ foldl (\acc m -> m.devisor * acc) 1 monkeys
+      -- Define an inspectFn function that modifies the WorryLevel of each Monkey.
+      inspectFn wl =
+        if part == 1 then
+          -- For part 1, divide the WorryLevel by 3 and convert it to a BigInt.
+          BigInt.fromInt $ (fromMaybe 0 $ Int.fromString $ BigInt.toString wl) / 3
+        else
+          -- For part 2, just return the original WorryLevel.
+          wl
+      -- Define a runRound function that applies the inspectItems function to each monkey.
+      runRound ms' = foldl (inspectItems inspectFn modulo) ms' ms'
+  -- Finally, apply the runRound function numRounds times to the monkeys array.
+  in foldl (\ms _ -> runRound ms) monkeys $ replicate numRounds unit
 
 inspectItems :: (WorryLevel -> WorryLevel) -> BigInt -> Array Monkey -> Monkey -> Array Monkey
 inspectItems fn modulo monkeys monkey =
-  foldl inspectItem monkeys items
+  -- `items` is an array of the items held by `monkey`
+  let items = (fromMaybe monkey $ monkeys !! monkey.id).items
+  -- Apply `inspectItem` to each item in `items` and update the `monkeys` array
+  in foldl inspectItem monkeys items
   where
-  items = (fromMaybe monkey $ monkeys !! monkey.id).items
+  -- `inspectItem` takes in an array of `Monkey` objects `monkeys'` and an `Item` object `item`.
+  -- It returns the updated `monkeys'` array with `item` possibly moved to another monkey.
   inspectItem monkeys' item =
+    -- Apply the function `fn` to the item's `WorryLevel`, then take the modulus with `modulo`.
+    -- Use the result to determine which monkey (if any) to move the item to.
     uncurry (moveItem monkeys' monkey.id) $ monkey.test $ fn $ monkey.op item `mod` modulo
 
+-- | Move an item from one monkey to another, returning a new array of monkeys.
 moveItem :: Array Monkey -> Id -> Id -> WorryLevel -> Array Monkey
 moveItem monkeys from to item = fromMaybe monkeys do
+-- Use fromMaybe to handle the case where the array index is out of bounds
+-- Get the monkey at the "from" index and the monkey at the "to" index
   fromMonkey <- monkeys !! from
   toMonkey <- monkeys !! to
+-- Create new versions of the from and to monkeys, with the appropriate changes
   let fromMonkey' = fromMonkey { items = drop 1 fromMonkey.items, itemsCount = fromMonkey.itemsCount + BigInt.fromInt 1 }
   let toMonkey' = toMonkey { items = toMonkey.items <> [ item ] }
+-- Create a new array of monkeys with the updated from and to monkeys
   pure $ map (\m -> if m.id == from then fromMonkey' else if m.id == to then toMonkey' else m) monkeys
-
