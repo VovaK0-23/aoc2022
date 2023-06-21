71e88f2021c5fdb53f9488178d6fe5bb500d259f
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-06-21 20:43:53 +0200

    Day 17 (Julia) - Refactor, add Makefile and README

--- d17_julia/Main.jl
+++ d17_julia/Main.jl
@@ -1,60 +1,62 @@
 using LinearAlgebra
 using Printf
+show = "--show" in ARGS
+if show
+    import Pkg
+    if Base.find_package("Plots") == nothing
+        Pkg.add("Plots")
+    end
+    using Plots
+    default(legend = false, c =:viridis, xticks=[], yticks=[])
+end
+
 
 const TOWER_WIDTH = 7
 const PADDING = (x = 2, y = 3)
 
 mutable struct Tower
     data::Matrix{Bool}
-    heights::Vector{Union{Int64,Nothing}}  # Store height by column
+    bottom::Bool
 end
 
-function Tower(height)
-    Tower(fill(false, height, TOWER_WIDTH), [nothing for _ = 1:TOWER_WIDTH])
-end
+Tower(height) = Tower(fill(false, height, TOWER_WIDTH), true)
 
 function update_tower!(tower::Tower, matrix::BitMatrix)
-    min = typemax(Int64)
-    max = 0
+    min, max = typemax(Int64), 0
+    tower.bottom = false
+
     for coord in argmax(matrix, dims = 1)
         row, col = Tuple(coord)
-        tower.heights[col] = if any(matrix[:, col])
-            if min > row
-                min = row
-            end
-            if max !== nothing && max < row
-                max = row
-            end
-            row
+        if any(matrix[:, col])
+            min = min > row ? row : min
+            max = (!tower.bottom && max < row) ? row : max
         else
-            max = nothing
-            nothing
+            tower.bottom = true
         end
     end
-    tower.data = matrix[min:(max !== nothing ? max + 8 : end), :]
+
+    tower.data = matrix[min:(!tower.bottom ? max + 8 : end), :]
 end
 
 struct Rocks
     data::Vector{Matrix{Bool}}
 end
 
-function Rocks(rocks::Vector{Vector{String}})
-    Rocks([
-        [
-            (idx = i - PADDING.x; idx >= 1 && idx <= length(block) && block[idx] == '#') for
-            block in rock, i = 1:TOWER_WIDTH
-        ] for rock in rocks
-    ])
-end
+Rocks(rocks::Vector{Vector{String}}) = Rocks([
+    [
+        (idx = i - PADDING.x; idx >= 1 && idx <= length(block) && block[idx] == '#') for
+        block in rock, i = 1:TOWER_WIDTH
+    ] for rock in rocks
+])
+
+get_rock(rocks::Rocks, i::Int) = rocks.data[mod(i - 1, length(rocks.data))+1]
 
 struct Jets
     buffer::Vector{Char}
     index::Ref{Int}
 end
 
-function Jets(jets::Vector{Char})
-    Jets(jets, Ref(1))
-end
+Jets(jets::Vector{Char}) = Jets(jets, Ref(1))
 
 function next_jet!(jets::Jets)
     jet = jets.buffer[jets.index[]]
@@ -63,21 +65,8 @@ function next_jet!(jets::Jets)
     jet
 end
 
-function main()
-    filepath = ARGS[1]
-    jets = Jets(read_file(filepath))
-    part1 = calculate_tower_height(jets, 2022)
-    print("Part 1: ")
-    println(@sprintf("%.f", part1))
-
-    jets.index[] = 1
-    part2 = calculate_tower_height(jets, 1000000000000)
-    print("Part 2: ")
-    println(@sprintf("%.f", part2))
-end
-
 function read_file(filepath)
-    println(filepath)
+    println("Input file: ", filepath)
     file = open(filepath, "r")
     contents = rstrip(read(file, String))
     close(file)
@@ -93,39 +82,35 @@ function calculate_tower_height(jets, num_rocks)
     tower = Tower(0)
     height = 0
 
-    remainder = 0
     divisor = 0
-    cache_height = 0
+    cached_index, cached_height = 0, 0
 
     for i = 1:num_rocks
-        rock_index = mod(i - 1, length(rocks.data)) + 1
-        rock = rocks.data[rock_index]
-
+        rock = get_rock(rocks, i)
         height = simulate_rock_falling(rock, jets, tower, height)
+
+        if show && i % 2 == 0
+          display(heatmap((tower.data), yflip=true, aspect_ratio = 1))
+        end
 
         cache_key = (tower.data, jets.index[])
         if cache_key in keys(tower_cache)
-            (remainder, cache_height) = tower_cache[cache_key]
-            divisor = i - remainder
+            (cached_index, cached_height) = tower_cache[cache_key]
+            divisor = i - cached_index
             break
         else
             tower_cache[cache_key] = (i, height)
         end
     end
 
-    if cache_height > 0
-        closest_num = num_rocks - (num_rocks % divisor) + remainder
+    if cached_height > 0
+        closest_num = num_rocks - divisor - (num_rocks % divisor) + cached_index
+        height =
+            (height - cached_height) * ((closest_num - cached_index) / divisor) +
+            cached_height
 
-        if closest_num > num_rocks
-            closest_num -= divisor
-        end
-
-        repeats = (closest_num - remainder) / divisor
-        height = (height - cache_height) * repeats + cache_height
         for i = closest_num+1:num_rocks
-            rock_index = mod(i - 1, length(rocks.data)) + 1
-            rock = rocks.data[rock_index]
-
+            rock = get_rock(rocks, i)
             height = simulate_rock_falling(rock, jets, tower, height)
         end
     end
@@ -138,49 +123,60 @@ function simulate_rock_falling(rock, jets, tower, height)
     add_rows = PADDING.y + rock_nrows
     tower.data = vcat(fill(false, add_rows, TOWER_WIDTH), tower.data)
     nrows = size(tower.data, 1)
-    bufTower = fill(false, nrows, TOWER_WIDTH)
-    bufTower[1:rock_nrows, :] = rock
+    current_rock = fill(false, nrows, TOWER_WIDTH)
+    current_rock[1:rock_nrows, :] = rock
     new_height = add_rows + height
 
-    shifted = false
+    shifted_down = true
     while true
-        if shifted
-            if (nothing in tower.heights && any(bufTower[nrows, :]))
-                break
-            end
-            buf = circshift(bufTower, (1, 0))
-            if any(buf .& tower.data)
-                break
-            end
-            if height < new_height
-                new_height -= 1
-            end
-            bufTower = buf
-            shifted = false
-        else
-            jet = next_jet!(jets)
-            if jet == '>'
-                if !(true in bufTower[:, TOWER_WIDTH])
-                    buf = circshift(bufTower, (0, 1))
+        if shifted_down
+            if next_jet!(jets) == '>'
+                if !(true in current_rock[:, TOWER_WIDTH])
+                    buf = circshift(current_rock, (0, 1))
                     if !any(buf .& tower.data)
-                        bufTower = buf
+                        current_rock = buf
                     end
                 end
             else
-                if !(true in bufTower[:, 1])
-                    buf = circshift(bufTower, (0, -1))
+                if !(true in current_rock[:, 1])
+                    buf = circshift(current_rock, (0, -1))
                     if !any(buf .& tower.data)
-                        bufTower = buf
+                        current_rock = buf
                     end
                 end
             end
-            shifted = true
+            shifted_down = false
+        else
+            if tower.bottom && any(current_rock[nrows, :])
+                break
+            end
+            buf = circshift(current_rock, (1, 0))
+            if any(buf .& tower.data)
+                break
+            end
+            new_height -= (height < new_height) ? 1 : 0
+            current_rock = buf
+            shifted_down = true
         end
     end
 
-    update_tower!(tower, (bufTower .| tower.data))
+    update_tower!(tower, (current_rock .| tower.data))
 
     return new_height
 end
 
-main()
+if length(ARGS) > 0
+    filepath = ARGS[1]
+    jets = Jets(read_file(filepath))
+
+    part1 = calculate_tower_height(jets, 2022)
+    print("Part 1: ")
+    println(@sprintf("%.f", part1))
+
+    jets.index[] = 1
+    part2 = calculate_tower_height(jets, 1000000000000)
+    print("Part 2: ")
+    println(@sprintf("%.f", part2))
+else
+    println("Usage: julia Main.jl <input-path> --show")
+end
---
+++ d17_julia/README.md
@@ -0,0 +1,22 @@
+# [Advent of Code 2022 Day 17](https://adventofcode.com/2022/day/17)
+## Solution in [Julia](https://julialang.org/)
+
+## Tested on 
+
+```console
+$ julia -v
+julia version 1.9.0
+```
+
+## Expected Result
+
+```console
+Input file: input.txt
+Part 1: 3209
+Part 2: 1580758017509
+```
+
+## Quick Start
+- install [julia](https://julialang.org/downloads/) and make sure `julia` is availible in `$PATH`
+- `$ make`
+  - `$ make show` to display visualization (runs appx 50s)
