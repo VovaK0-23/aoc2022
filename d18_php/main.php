3f443cc7a46b746065dace7bf2f4632fc2cd3f3f
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-06-26 17:21:10 +0200

    Day 18 (PHP) - Add part 2

--- d18_php/main.php
+++ d18_php/main.php
@@ -1,24 +1,53 @@
 <?php
 if (count($argv) > 1) {
   $filepath = $argv[1];
+  $result = parse_file($filepath);
+  $cubes = $result['cubes'];
+  $min = $result['min'];
+  $max = $result['max'];
+  $water_min = array_map(fn($value) => $value - 1, $min);
+  $water_max = array_map(fn($value) => $value + 1, $max);
 
-  $cubes = parse_file($filepath);
+  part1($cubes);
+  part2($cubes, $water_min, $water_max);
+} else {
+  echo "Usage: php main.php <input-path>\n";
+}
 
+function parse_file($filepath)
+{
+  echo 'Input file: ', $filepath, "\n";
+
+  $cubes = [];
+  $file = fopen($filepath, 'r');
+  $min = [PHP_INT_MAX, PHP_INT_MAX, PHP_INT_MAX];
+  $max = [0, 0, 0];
+
+  // Read the file line by line using fgets()
+  while (($line = fgets($file)) !== false) {
+    $cube = explode(',', str_replace("\n", '', $line));
+    $cubes[] = $cube;
+    for ($i = 0; $i <= 2; $i++) {
+      if ($cube[$i] < $min[$i]) {
+        $min[$i] = $cube[$i];
+      }
+      if ($cube[$i] > $max[$i]) {
+        $max[$i] = $cube[$i];
+      }
+    }
+  }
+
+  fclose($file);
+
+  return ['cubes' => $cubes, 'min' => $min, 'max' => $max];
+}
+
+function part1($cubes)
+{
   $surfaceArea = 0;
 
   foreach ($cubes as $cube) {
-    $x = $cube[0];
-    $y = $cube[1];
-    $z = $cube[2];
-
-    $neighbors = [
-      [$x, $y - 1, $z], // Up
-      [$x, $y + 1, $z], // Down
-      [$x - 1, $y, $z], // Left
-      [$x + 1, $y, $z], // Right
-      [$x, $y, $z - 1], // Front
-      [$x, $y, $z + 1], // Back
-    ];
+    $neighbors = build_neighbors($cube);
 
     foreach ($neighbors as $neighbor) {
       if (!in_array($neighbor, $cubes)) {
@@ -27,24 +56,60 @@ if (count($argv) > 1) {
     }
   }
 
-  echo $surfaceArea . "\n";
-} else {
-  echo "Usage: php main.php <input-path>\n";
+  echo 'Part 1: ', $surfaceArea, "\n";
 }
 
-function parse_file($filepath)
+function part2($lava_cubes, $min, $max)
 {
-  echo 'Input file: ' . $filepath . "\n";
+  $q = [$min];
+  $water_cubes = [];
+  $surfaceArea = 0;
 
-  $cubes = [];
-  $file = fopen($filepath, 'r');
+  while (!empty($q)) {
+    $cube = array_shift($q);
+    if (in_array($cube, $water_cubes)) {
+      continue;
+    }
+    $water_cubes[] = $cube;
+    $neighbors = build_neighbors($cube);
 
-  // Read the file line by line using fgets()
-  while (($line = fgets($file)) !== false) {
-    $cubes[] = explode(',', str_replace("\n", '', $line));
+    foreach ($neighbors as $neighbor) {
+      if (valid_cube($neighbor, $min, $max)) {
+        if (!in_array($neighbor, $lava_cubes)) {
+          array_push($q, $neighbor);
+        } else {
+          $surfaceArea++;
+        }
+      }
+    }
   }
 
-  fclose($file);
+  echo 'Part 2: ', $surfaceArea, "\n";
+}
 
-  return $cubes;
+function build_neighbors($cube)
+{
+  $x = $cube[0];
+  $y = $cube[1];
+  $z = $cube[2];
+
+  return [
+    [$x, $y - 1, $z], // Up
+    [$x, $y + 1, $z], // Down
+    [$x - 1, $y, $z], // Left
+    [$x + 1, $y, $z], // Right
+    [$x, $y, $z - 1], // Front
+    [$x, $y, $z + 1], // Back
+  ];
+}
+
+function valid_cube($cube, $min, $max)
+{
+  $valid = true;
+  for ($i = 0; $i <= 2; $i++) {
+    if ($cube[$i] < $min[$i] ||  $max[$i] < $cube[$i]) {
+      return false;
+    }
+  }
+  return true;
 }
